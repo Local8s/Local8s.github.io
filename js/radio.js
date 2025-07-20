2d8289feeed7deb45da3b21e2d7ef1702994e808
Local8s <local8sfm@gmail.com>
2025-07-19 23:40:04 -0700

    Update radio.js

--- js/radio.js
+++ js/radio.js
@@ -32,7 +32,7 @@
       },
       {
         src: 'https://file.garden/aGGJpwNLWActgKKi/032125_HOUSE_L8.mp3',
-        name: 'L8s RADIO NEW! 89.5FM'
+        name: 'L8s RADIO 89.5FM'
       },
      {
         src: 'https://file.garden/aGGJpwNLWActgKKi/070725_ELECTRO_L8s.mp3',
@@ -41,6 +41,10 @@
        {
         src: 'https://file.garden/aGGJpwNLWActgKKi/071725_MIX_L8.mp3',
         name: 'L8s RADIO NEW! 93.3FM'
+      },
+       {
+        src: 'https://file.garden/aGGJpwNLWActgKKi/071925_ALT_L8.mp3',
+        name: 'L8s RADIO NEW! 96.8FM'
       }
     ];
 
