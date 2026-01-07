c609df2a9cf21c5b180b12df2cdc723459a433ce
Local 8's Radio <local8sfm@gmail.com>
2026-01-06 17:42:19 -0800

    Update radio station name with additional text

--- js/test/radio.js
+++ js/test/radio.js
@@ -15,7 +15,7 @@
       },
        {
         src: 'https://file.garden/aGGJpwNLWActgKKi/060325_MIX_L8.mp3',
-        name: 'ABCDEFGHIJ 101.5FM'
+        name: 'AB CDEFG HIJjasj abhasjkacac asjbas 101.5FM'
       },
       {
         src: 'https://file.garden/aGGJpwNLWActgKKi/040825_DISCO_L8.mp3',
