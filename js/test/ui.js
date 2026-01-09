88c5c4f85a82a0f2192f221e397d873cb1611434
Local 8's Radio <local8sfm@gmail.com>
2026-01-09 10:42:07 -0800

    Update ui.js

--- js/test/ui.js
+++ js/test/ui.js
@@ -147,3 +147,18 @@ document.querySelectorAll('.station').forEach(el => {
 
   window.addEventListener('load', startMarquee);
   window.addEventListener('resize', startMarquee);
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
